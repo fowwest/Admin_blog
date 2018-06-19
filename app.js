987981eafe785eb7f7620486fa60e6362b515f64
Forrest <forrestiscarlton@gmail.com>
2018-06-18 20:48:24 -0400

    Added image api routes to blog.js

--- app.js
+++ app.js
@@ -41,33 +41,6 @@ app.use('/', admin);
 app.use('/', routes);
 app.use('/api', blogApi);
 
-//URL : http://localhost:3000/images/
-// To get all the images/files stored in MongoDB
-app.get('/images', function(req, res) {
-//calling the function from index.js class using routes object..
-routes.getImages(function(err, genres) {
-if (err) {
-throw err;
- 
-}
-res.json(genres);
- 
-});
-});
- 
-// URL : http://localhost:3000/images/(give you collectionID)
-// To get the single image/File using id from the MongoDB
-app.get('/images/:id', function(req, res) {
- 
-//calling the function from index.js class using routes object..
-routes.getImageById(req.params.id, function(err, genres) {
-if (err) {
-throw err;
-}
-//res.download(genres.path);
-res.send(genres.path)
-});
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/routes/blog.js
+++ app_api/routes/blog.js
@@ -1,8 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var ctrlBlog = require('../controllers/blog');
+var routes = require('../../app_server/routes/imageFile');
 
 // Blog
 router.get('/blog', ctrlBlog.getBlogPosts);
+
+//URL : http://localhost:3000/images/
+// To get all the images/files stored in MongoDB
+router.get('/images', function(req, res) {
+//calling the function from index.js class using routes object..
+routes.getImages(function(err, genres) {
+if (err) {
+throw err;
+ 
+}
+res.json(genres);
+ 
+});
+});
+ 
+// URL : http://localhost:3000/images/(give you collectionID)
+// To get the single image/File using id from the MongoDB
+router.get('/images/:id', function(req, res) {
+ 
+//calling the function from index.js class using routes object..
+routes.getImageById(req.params.id, function(err, genres) {
+if (err) {
+throw err;
+}
+//res.download(genres.path);
+res.send(genres.path)
+});
+});
 
 module.exports = router;
