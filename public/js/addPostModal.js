db984222f7fd64bc9526684e73d4eb8b5452be89
Forrest <forrestiscarlton@gmail.com>
2018-08-05 00:25:31 -0400

    Added ability to delete posts

--- public/js/addPostModal.js
+++ public/js/addPostModal.js
@@ -1,5 +1,5 @@
 // Get the modal
-var addPostModal = document.getElementById('modalAddPost');
+var addPostModal = document.getElementById('addPostModal');
 // Get the button that opens the modal
 var btn = document.getElementById("addPostBtn");
 // Get the <span> element that closes the modal
@@ -11,10 +11,4 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
       addPostModal.style.display = "none";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-      if (event.target == modal) {
-            addPostModal.style.display = "none";
-      }
 };
--- public/js/deletePostModal.js
+++ public/js/deletePostModal.js
@@ -1,20 +1,19 @@
 // Get the modal
 var deletePostModal = document.getElementById('deletePostModal');
+
 // Get the button that opens the modal
-var btn = document.getElementById("deletePostBtn");
+var deletePostBtn = document.getElementById('#{post._id}');
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closeDeletePost")[0];
+
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+deletePostBtn.onclick = function() {
       deletePostModal.style.display = "block";
 };
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
       deletePostModal.style.display = "none";
 };
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-      if (event.target == modal) {
-            deletePostModal.style.display = "none";
-      }
-};
+
